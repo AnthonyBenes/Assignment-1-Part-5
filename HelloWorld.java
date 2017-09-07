830bac24bce3200b7e8aa6e6e6cfb2d13e37e26b
Anthony Benes <31495775+AnthonyBenes@users.noreply.github.com>
2017-09-06 23:02:55 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,5 @@
 public class HelloWorld {
     public static void main(String args[]) {
-        int numberOfStudents = 10;
-        System.out.println("Hello World!");
-        numberOfStudents = 20;
-        System.out.println(numberOfStudents);
+        System.out.println("Anthoyn Benes!!!");
     }
 }
